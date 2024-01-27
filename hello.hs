a7bdf9ffe59bac57312121c93815a1fbb9f43c59
Alister <alister7@gmail.com>
2024-01-27 10:18:08 +0900

    feat: working on html edsl

--- hello.hs
+++ hello.hs
@@ -1,34 +1,50 @@
 -- hello.hs
 
 main :: IO ()
-main = putStrLn myHtml
+main = putStrLn (render myHtml)
 
-myHtml = makeHtml
-  "Homepage"
-  (h1_ "Title" <> p_ "Paragraph")
+myHtml :: Html
+myHtml =
+  html_
+    "Homepage"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph 1")
+        (p_ "Paragraph 2")
+      )
+    )
 
-makeHtml :: String -> (String -> String)
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+newtype Html = Html String
 
-html_ :: String -> String
-html_ = el "html"
+newtype Structure = Structure String
 
-body_ :: String -> String
-body_ = el "body"
+type Title = String
 
-head_ :: String -> String
-head_ = el "head"
+html_ :: Title -> Structure -> Html
+html_ title content = Html ( el "html"
+  (el "head" (el "title" title) <> el "body" (getStructureString content) ))
 
-title_ :: String -> String
-title_ = el "title"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+append_ :: Structure -> Structure -> Structure
+append_ a b =
+  Structure(getStructureString a <> getStructureString b)
+
+getStructureString :: Structure -> String
+getStructureString content =
+  case content of
+    Structure str -> str
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
