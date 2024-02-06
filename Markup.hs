1d221672e6f781f3d60e22ca3226f2ea22d8fe78
Alister <alister7@gmail.com>
2024-02-06 17:22:42 +0900

    feat: add more markdown support

--- Markup.hs
+++ Markup.hs
@@ -1,11 +1,17 @@
 -- Markup.hs
 
-module Markup (Document, Structure (..)) where
+module Markup
+  ( Document
+  , Structure(..)
+  , parse
+  )
+where
 
 import Numeric.Natural
 import Data.Maybe (maybeToList)
 
-type Document = [Structure]
+type Document
+  = [Structure]
 
 data Structure
   = Heading Natural String
@@ -13,7 +19,7 @@ data Structure
   | UnorderedList [String]
   | OrderedList [String]
   | CodeBlock [String]
-  deriving Show
+  deriving (Eq, Show)
 
 parse :: String -> Document
 parse = parseLines Nothing . lines
@@ -21,7 +27,40 @@ parse = parseLines Nothing . lines
 parseLines :: Maybe Structure -> [String] -> Document
 parseLines context txts =
   case txts of
+    -- done case
     [] -> maybeToList context
+
+    -- Heading 1 case
+    ('#' : ' ' : line) : rest ->
+      maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+
+    -- Unordered list case
+    ('-' : ' ' : line) : rest ->
+      case context of
+        Just (UnorderedList list) ->
+          parseLines (Just (UnorderedList (list <> [trim line]))) rest
+
+        _other ->
+          maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
+
+    -- Ordered list case
+    ('*' : ' ' : line) : rest ->
+      case context of
+        Just (OrderedList list) ->
+          parseLines (Just (OrderedList (list <> [trim line]))) rest
+
+        _other ->
+          maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
+
+    -- Code block case
+    ('>' : ' ' : line) : rest ->
+      case context of
+        Just (CodeBlock code) ->
+          parseLines (Just (CodeBlock (code <> [line]))) rest
+
+        _other ->
+          maybe id (:) context (parseLines (Just (CodeBlock [line])) rest)
+
     -- Paragraph case
     currentLine : rest ->
       let
@@ -34,12 +73,9 @@ parseLines context txts =
             case context of
               Just (Paragraph paragraph) ->
                 parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
-              _ ->
+              _other ->
                 maybe id (:) context (parseLines (Just (Paragraph line)) rest)
 
 trim :: String -> String
 trim = unwords . words
-
-print :: Show a => a -> IO ()
-print = putStrLn . show
 
---
+++ test.md
@@ -0,0 +1,12 @@
+# A test file
+
+- for testing
+- the feature
+- of the app
+
+# heading number 2
+
+> also code blocks
+> more code
+
+and a simple paragraph
